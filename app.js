b777b3468976f8737aef32263a110a9b490abe41
Serhii Hloba <globasergey29@gmail.com>
2020-03-11 14:41:59 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+$(function () {
+    /*Fixed header*/
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    checkScroll(scrollPos, introH);
+
+    $(window).on("scroll resize", function () {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+        checkScroll(scrollPos, introH);
+    });
+
+    function checkScroll(scrollPos, introH) {
+        if (scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    };
+
+    /*Smooth */
+
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+        console.log(elementOffset);
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 60
+        }, 700);
+    });
+
+    /* Nav Toggle */
+    let nav = $("#nav");
+    let navToggle =$("#navToggle");
+
+    $("#navToggle").on("click", function (event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    });
+});
